41233b781f633fc45b1abfa17eb641903b1108f5
Rohan <rohanbhokare92@gmail.com>
2019-03-11 18:14:18 +0530

    basicSetupOfCatalogApi

---
+++ src/Services/ProductCatalogApi/Data/CatalogContext.cs
@@ -0,0 +1,67 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductCatalogApi.Domain;
+
+namespace ProductCatalogApi.Data
+{
+    public class CatalogContext: DbContext
+    {
+        public CatalogContext(DbContextOptions options): base(options)
+        {
+             
+        }
+
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+        public DbSet<CatalogItem> CatalogItems  { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CatalogBrand>(ConfigureCatalogBrand);
+            modelBuilder.Entity<CatalogType>(ConfigureCatalogType);
+            modelBuilder.Entity<CatalogItem>(ConfigureCatalogItem);
+
+
+        }
+
+        private void ConfigureCatalogItem(EntityTypeBuilder<CatalogItem> builder)
+        {
+            builder.ToTable("Catalog");
+            builder.Property(c => c.Id)
+                .ForSqlServerUseSequenceHiLo("catalog_hilo")
+                .IsRequired(true);
+            builder.Property(c => c.Name)
+                .IsRequired(true)
+                .HasMaxLength(50);
+            builder.Property(c => c.Price)
+                .IsRequired(true);
+            builder.Property(c => c.PictureUrl)
+                .IsRequired(false);
+            builder.HasOne(x => x.CatalogBrand).WithMany().HasForeignKey(x => x.CatalogBrandId);
+            builder.HasOne(x => x.CatalogType).WithMany().HasForeignKey(x => x.CatalogTypeId);
+        }
+
+        private void ConfigureCatalogType(EntityTypeBuilder<CatalogType> builder)
+        {
+            builder.ToTable("CatalogType");
+            builder.Property(c => c.Id)
+                .ForSqlServerUseSequenceHiLo("catalog_type_hilo")
+                .IsRequired();
+            builder.Property(c => c.Type)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+
+        private void ConfigureCatalogBrand(EntityTypeBuilder<CatalogBrand> builder)
+        {
+            builder.ToTable("CatalogBrand");
+            builder.Property(c => c.Id)
+                .ForSqlServerUseSequenceHiLo("catalog_brand_hilo")
+                .IsRequired();
+            builder.Property(c => c.Brand)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
---
+++ src/Services/ProductCatalogApi/Domain/CatalogBrand.cs
@@ -0,0 +1,8 @@
+namespace ProductCatalogApi.Domain
+{
+    public class CatalogBrand
+    {
+        public int Id { get; set; } 
+        public string Brand   { get; set; }
+    }
+}
